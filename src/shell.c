12dbe4b20ed4210e98dc61f29bc608b2c2e2c842
inspireEU <maxhagan@hotmail.co.uk>
2023-01-31 09:50:11 +0000

    added return - Max

--- src/shell.c
+++ src/shell.c
@@ -3,3 +3,40 @@
 // Date: 31/01/2023
 
 #include <stdio.h>
+
+int main(void) {
+
+    // SHELL EXECUTION SPEC
+
+    // TODO: Find the user home directory from the environment
+
+    // TODO: Set current working directory to user home directory
+
+    // TODO: Save the current path
+
+    // TODO: Load history
+
+    // TODO: Load aliases
+
+    // TODO: Do while shell has not terminated
+        // TODO: Display prompt
+
+        // TODO: Read and parse user input
+
+        // TODO: While the command is a history invocation or alias then replace it with the
+        // appropriate command from history or the aliased command respectively
+
+        // TODO: If command is built-in invoke appropriate function
+
+        // TODO: Else execute command as an external process
+
+    // TODO: End while
+
+    // TODO: Save history
+
+    // TODO: Save aliases
+
+    // TODO: Restore original path
+
+    return 0;
+}
