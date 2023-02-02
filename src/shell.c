a5c17b0a138c7c01cd2bf75d80804a39be329fe7
Calum <57592605+calum-s@users.noreply.github.com>
2023-02-02 13:13:52 +0000

    Set and get home dir, get usr input, CTRL D - calum

--- src/shell.c
+++ src/shell.c
@@ -2,15 +2,23 @@
 // Authors: Kyle Pereria, Calum Scott, Karim Moukaouame, Max Hagan, Peter King
 // Date: 31/01/2023
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+void loadFiles(void) {
+    
+}
 
 int main(void) {
 
     // SHELL EXECUTION SPEC
 
     // TODO: Find the user home directory from the environment
+    char* home = getenv("HOME");
 
-    // TODO: Set current working directory to user home directory
+    // Set current working directory to home variable
+    chdir(home);
 
     // TODO: Save the current path
 
@@ -18,19 +26,26 @@ int main(void) {
 
     // TODO: Load aliases
 
+    while(1) {
     // TODO: Do while shell has not terminated
         // TODO: Display prompt
+        printf("> ");
 
         // TODO: Read and parse user input
+
+        // Read user input
+        char input[1024];
+        if (fgets(input, 1024, stdin) == NULL) {
+            exit(0);
+        }
+
 
         // TODO: While the command is a history invocation or alias then replace it with the
         // appropriate command from history or the aliased command respectively
 
         // TODO: If command is built-in invoke appropriate function
-
         // TODO: Else execute command as an external process
-
-    // TODO: End while
+    }
 
     // TODO: Save history
 
