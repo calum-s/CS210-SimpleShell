90aece24f152c92da1fce6734951b61d2f19a17e
Karim <72822543+kazav@users.noreply.github.com>
2023-02-02 13:07:14 +0000

    Update shell.c
    fixing my name :)

--- src/shell.c
+++ src/shell.c
@@ -1,5 +1,5 @@
 // Project: Simple Shell
-// Authors: Kyle Pereria, Calum Scott, Karim Moukaoume, Max Hagan, Peter King
+// Authors: Kyle Pereria, Calum Scott, Karim Moukaouame, Max Hagan, Peter King
 // Date: 31/01/2023
 
 #include <stdio.h>
