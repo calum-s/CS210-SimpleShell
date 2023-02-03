5948f8dc2c3b54c2acc0b0c907578d4e5e718220
x-yl <kylepereira@mail.com>
2023-02-03 18:06:46 +0000

    Correctly execute external programs

---
+++ src/command.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include "token.h"
+
+void start_external(TokenList* tokens);
+void sigint(int);
+
--- src/shell.c
+++ src/shell.c
@@ -7,10 +7,20 @@
 #include <stdio.h>
 #include <string.h>
 #include "token.h"
+#include "command.h"
+#include <signal.h>
+#include <errno.h>
+#include <stdbool.h>
 
 int main(void) {
-
     // SHELL EXECUTION SPEC
+
+    struct sigaction act = {
+            .sa_handler = sigint,
+            .sa_flags = 0,
+    };
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGINT, &act, NULL);
 
     // TODO: Find the user home directory from the environment
     char* home = getenv("HOME");
@@ -29,15 +39,22 @@ int main(void) {
         // TODO: Display prompt
         printf("> ");
 
-        // TODO: Read and parse user input
-
         // Read user input
         char* input = malloc(64);
         size_t input_size = 64;
         size_t offset = 0;
+        bool force_continue = false;
 
         while (1) {
             if (fgets(input + offset, (int) (input_size - offset), stdin) == NULL) {
+                if (!feof(stdin)) {
+                    // Happens on SIGINT which should cause line to be discarded.
+                    printf("\n");
+                    free(input);
+                    force_continue = true;
+                    break;
+                }
+                free(input);
                 exit(0);
             }
 
@@ -49,9 +66,12 @@ int main(void) {
                 break;
             }
         }
+        if (force_continue) continue;
 
         TokenList tokens = tokenize(input);
         if (tokens.size == 0) {
+            free_token_list(&tokens);
+            free(input);
             continue;
         }
         if (tokens.size == 1 && strncmp(tokens.tokens[0].start, "exit", 4) == 0) {
@@ -60,14 +80,12 @@ int main(void) {
             exit(0);
         }
 
-        print_token_list(&tokens);
-
         // TODO: While the command is a history invocation or alias then replace it with the
         // appropriate command from history or the aliased command respectively
 
         // TODO: If command is built-in invoke appropriate function
-        // TODO: Else execute command as an external process
-        
+
+        start_external(&tokens);
         free_token_list(&tokens);
         free(input);
     }
