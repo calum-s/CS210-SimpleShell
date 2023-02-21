390951ea96a17e13c0755683d2c1dedc525ed543
inspireEU <maxhagan@hotmail.co.uk>
2023-02-21 09:52:30 +0000

    added handling for different working dirs;

--- src/file.c
+++ src/file.c
@@ -31,13 +31,15 @@ void open_file(const char* fileName) {
     FILE* file = fopen(fileName, "r");
 
     if (file == NULL) {                // check if the file is valid/exists
-        char* dirname = "shellconfig"; // set the directory name for first-time creation
+        char dirname[100];
+        snprintf(dirname, 100, "%s%s", getenv("HOME"), "/shellconfig"); // set the directory name for first-time creation in working directory
 
         if (mkdir(dirname, 0775) < 0 && errno != EEXIST) { // check if the directory was created
             perror("mkdir");
             return;
         }
-        file = fopen(fileName, "a");
+
+        file = fopen(fileName, "a"); // creates file if it does not already exist
     }
 
     Command allCommands;
